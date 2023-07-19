6217c54a9273e5bf8a365500dfb93d33c991ea75
Dimitar Lazarov <117237901+salaj80@users.noreply.github.com>
2023-07-19 15:32:35 +0200

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
   const meals = ref([]);
   
   onMounted(async () => {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 9; i++) {
       axiosClient
         .get(`random.php`)
         .then(({ data }) => meals.value.push(data.meals[0]));
